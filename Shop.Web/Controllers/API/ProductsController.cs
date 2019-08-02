ddbd6fae3da22ecd3eb7867dda6b7e219c50c068
Luis José Mazariego <lmazariego@cgclatam.com>
2019-08-02 12:09:42 -0600

    API Products Controller

---
+++ Shop.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿namespace Shop.Web.Controllers.API
+{    
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Shop.Web.Data;
+
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(_productRepository.GetAll());
+        }
+    }
+}
