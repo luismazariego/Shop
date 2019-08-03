7af3ef09e90e28b8697fe417a8692774f32fcb63
Luis José Mazariego <lmazariego@cgclatam.com>
2019-08-03 11:58:54 -0600

    Xamarin Forms, Login page add nuget mvvm light

---
+++ Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
@@ -0,0 +1,57 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Shop.UIForms.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public ICommand LoginCommand => new RelayCommand(Login);
+
+        public LoginViewModel()
+        {
+            Email = "mazariego2011@gmail.com";
+            Password = "123456";
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an email",
+                    "Accept");
+                return;
+            }
+            if (string.IsNullOrEmpty(Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an Password",
+                    "Accept");
+                return;
+            }
+
+            if (!Email.Equals("mazariego2011@gmail.com") || !Password.Equals("123456"))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "User or Password Wrong",
+                    "Accept");
+                return;
+            }
+
+            await Application.Current.MainPage.DisplayAlert(
+                    "OK",
+                    "Fuck Yeah!!",
+                    "Accept");
+
+        }
+    }
+}
