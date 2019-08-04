46b57f5ce10907970a418bc5610c5b68c78574e0
Luis José Mazariego <lmazariego@cgclatam.com>
2019-08-04 00:38:36 -0600

    Change User and Password

--- Shop.Web/Helpers/UserHelper.cs
+++ Shop.Web/Helpers/UserHelper.cs
@@ -1,12 +1,9 @@
 ﻿namespace Shop.Web.Helpers
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Identity;
     using Shop.Web.Data.Entities;
-    using Shop.Web.Models;
+    using Shop.Web.Models;    
 
     public class UserHelper : IUserHelper
     {
@@ -14,35 +11,34 @@
         private readonly SignInManager<User> _signInManager;
 
         public UserHelper(
-            UserManager<User> userManager, 
+            UserManager<User> userManager,
             SignInManager<User> signInManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
         }
 
-        public async Task<IdentityResult> AddUserAsync(User user, string password)
-        {
-            return await _userManager.CreateAsync(user, password);
-        }
+        public async Task<IdentityResult> AddUserAsync(User user, string password) 
+            => await _userManager.CreateAsync(user, password);
 
-        public async Task<User> GetUserByEmailAsync(string email)
-        {
-            return await _userManager.FindByEmailAsync(email);
-        }
+        //Expression body for methods (like delegates)
+        public async Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword) 
+            => await _userManager.ChangePasswordAsync(user, oldPassword, newPassword);
 
-        public async Task<SignInResult> LoginAsync(LoginViewModel model)
-        {
-            return await _signInManager.PasswordSignInAsync(
+        public async Task<User> GetUserByEmailAsync(string email) 
+            => await _userManager.FindByEmailAsync(email);
+
+        public async Task<SignInResult> LoginAsync(LoginViewModel model) 
+            => await _signInManager.PasswordSignInAsync(
                 model.Username,
                 model.Password,
                 model.RememberMe,
                 false);
-        }
 
-        public async Task LogoutAsync()
-        {
-            await _signInManager.SignOutAsync();
-        }
+        public async Task LogoutAsync() 
+            => await _signInManager.SignOutAsync();
+
+        public async Task<IdentityResult> UpdateUserAsync(User user) 
+            => await _userManager.UpdateAsync(user);
     }
 }
---
+++ Shop.Web/Models/ChangePasswordViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Shop.Web.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [Display(Name = "Old Password")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [Display(Name = "New Password")]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [Compare("NewPassword")]
+        public string Confirm { get; set; }
+    }
+}
